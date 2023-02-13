27ad8294fb0db780451f9109dae8ae5802509918
Eric Gichuri <gichurieric50@gmail.com>
2023-02-13 15:46:51 +0300

    Update on my website

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 		}
 		
 	})
-	$(".btnseemore").click(function(){
+	$(".btnlink").click(function(){
 		var pid=$(this).attr("data")
 		if(pid==6){
 			window.location.href="projects.html"
@@ -47,9 +47,9 @@ $(document).ready(function(){
 })
 
 gsap.from('body',{duration:0,display:'none',delay:0,ease:'bounce'})
-gsap.from('.landing',{duration: 0.4, x:'-100vw',background:'#000088' ,delay:0.2,ease:'power2.in'})
-gsap.from('.text-landing',{duration:0.4,y:'50px',delay:0.7,opacity:0,ease:'power'})
-gsap.from('.img1',{duration:0.4,y:'50px',delay:0.7,opacity:0,ease:'power'})
+gsap.from('.landing',{duration: 0.3, x:'-100vw',background:'#000088' ,delay:0.2,ease:'power2.in'})
+gsap.from('.text-landing',{duration:0.4,y:'50px',delay:0.6,opacity:0,ease:'power'})
+gsap.from('.img1',{duration:0.4,y:'50px',delay:0.6,opacity:0,ease:'power'})
 gsap.to('.landing',{duration: 0.6, height:'0px',display:'none',delay:2,ease:'power2.in'})
 gsap.from('.navbar',{duration: 2,display:'none',opacity:'0',delay:3,ease:'power'})
 gsap.from('.container1',{duration:0.4,display:'none',y:'100%',delay:4,ease:'power2.inOut'})
@@ -58,13 +58,13 @@ gsap.from('.container1 .subcontainer2',{duration:0.8,x:'-100vw',delay:5.7,ease:'
 gsap.from('.container1 .subcontainer3',{duration:0.8,x:'100vw',delay:5.8,ease:'power'})
 gsap.from('.container1 .subcontainer4',{duration:0.8,x:'-100vw',delay:5.9,ease:'power'})
 
-gsap.from('img',{'duration':0.6,x:'-20px',opacity:0,delay:6,ease:'power2.in',stagger:.2})
-gsap.from('button',{'duration':0.6,x:'100vw',delay:6,ease:'power2.in',stagger:.2})
+gsap.from('img',{'duration':0.5,x:'-20px',opacity:0,delay:6,ease:'power2.in',stagger:.2})
+gsap.from('button',{'duration':0.5,x:'100vw',delay:6,ease:'power2.in',stagger:.2})
 
-gsap.from('.container1 h3',{duration:0.7,y:'50px',delay:4.4,opacity:0,ease:'power',stagger:.3})
-gsap.from('.container1 h4',{duration:0.7,y:'50px',delay:5.1,opacity:0,ease:'power',stagger:.3})
-gsap.from('.container1 h5',{duration:0.7,y:'50px',delay:5.7,opacity:0,ease:'power',stagger:.3})
-gsap.from('.container1 h6',{duration:0.7,y:'50px',delay:5.9,opacity:0,ease:'power',stagger:.3})
-gsap.from('.container1 p',{duration:0.7,y:'50px',delay:6.1,opacity:0,ease:'power',stagger:.3})
+gsap.from('.container1 h3',{duration:0.6,y:'50px',delay:4.4,opacity:0,ease:'power',stagger:.3})
+gsap.from('.container1 h4',{duration:0.6,y:'50px',delay:5.1,opacity:0,ease:'power',stagger:.3})
+gsap.from('.container1 h5',{duration:0.6,y:'50px',delay:5.7,opacity:0,ease:'power',stagger:.3})
+gsap.from('.container1 h6',{duration:0.6,y:'50px',delay:5.9,opacity:0,ease:'power',stagger:.3})
+gsap.from('.container1 p',{duration:0.6,y:'50px',delay:6.1,opacity:0,ease:'power',stagger:.3})
 
 gsap.from('.footer',{duration:2,display:'none',delay:6,ease:'power2.in'})
