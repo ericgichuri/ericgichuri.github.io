196304ba6752c7b0795a1395303b3272056dae87
Eric Gichuri <gichurieric50@gmail.com>
2023-02-27 16:47:52 +0300

    update on my website

--- scripts/index.js
+++ scripts/index.js
@@ -47,10 +47,10 @@ $(document).ready(function(){
 })
 
 gsap.from('body',{duration:0,display:'none',delay:0,ease:'bounce'})
-gsap.from('.landing',{duration: 0.3, x:'-100vw',background:'#000088' ,delay:0.2,ease:'power2.in'})
-gsap.from('.text-landing',{duration:0.4,y:'50px',delay:0.6,opacity:0,ease:'power'})
-gsap.from('.img1',{duration:0.4,y:'50px',delay:0.6,opacity:0,ease:'power'})
-gsap.to('.landing',{duration: 0.6, height:'0px',display:'none',delay:2,ease:'power2.in'})
+gsap.from('.landing',{duration: 0.3, x:'-100vw',background:'#000088' ,delay:0.0,ease:'power2.in'})
+gsap.from('.text-landing',{duration:0.4,y:'50px',delay:0.5,opacity:0,ease:'power'})
+gsap.from('.img1',{duration:0.4,y:'50px',delay:0.4,opacity:0,ease:'power'})
+gsap.to('.landing',{duration: 0.5, height:'0px',display:'none',delay:1.5,ease:'power2.in'})
 gsap.from('.navbar',{duration: 2,display:'none',opacity:'0',delay:3,ease:'power'})
 gsap.from('.container1',{duration:0.4,display:'none',y:'100%',delay:4,ease:'power2.inOut'})
 gsap.from('.container1 .subcontainer1',{duration:0.8,x:'100vw',delay:4.4,ease:'power'})
@@ -58,8 +58,8 @@ gsap.from('.container1 .subcontainer2',{duration:0.8,x:'-100vw',delay:5.7,ease:'
 gsap.from('.container1 .subcontainer3',{duration:0.8,x:'100vw',delay:5.8,ease:'power'})
 gsap.from('.container1 .subcontainer4',{duration:0.8,x:'-100vw',delay:5.9,ease:'power'})
 
-gsap.from('img',{'duration':0.5,x:'-20px',opacity:0,delay:6,ease:'power2.in',stagger:.2})
-gsap.from('button',{'duration':0.5,x:'100vw',delay:6,ease:'power2.in',stagger:.2})
+gsap.from('img',{'duration':0.5,x:'-20px',opacity:0,delay:6,ease:'power2.in',stagger:.1})
+gsap.from('button',{'duration':0.5,x:'100vw',delay:6,ease:'power2.inOut',stagger:.2})
 
 gsap.from('.container1 h3',{duration:0.6,y:'50px',delay:4.4,opacity:0,ease:'power',stagger:.3})
 gsap.from('.container1 h4',{duration:0.6,y:'50px',delay:5.1,opacity:0,ease:'power',stagger:.3})
