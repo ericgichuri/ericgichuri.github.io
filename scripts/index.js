ac2445b80ca6dc5e2aca9a79b8ed05be4e69f67e
Eric Gichuri <ericgichuri2605@gmail.com>
2023-08-12 16:48:50 +0300

    updated
    updated all the website

--- scripts/index.js
+++ scripts/index.js
@@ -1,70 +1,10 @@
-
 $(document).ready(function(){
-
-	$(".menubtnhol").click(function(){
-		btnstatus=$(this).attr("data")
-		if (btnstatus=="hidden"){
-			$(".navbar ul").addClass('show')
-			$(this).attr("data","show")
-			$(".menubtnhol img").attr("src","icons/close.png")
-		}else if(btnstatus=="show"){
-			$(".navbar ul").removeClass('show')
-			$(this).attr("data","hidden")
-			$(".menubtnhol img").attr("src","icons/menubtn.png")
-		}
-		
+	$(".menu_button").click(function(){
+		$(".menu_holder").toggleClass("show")
+		$(".menu_button span").toggleClass("fa-window-close fa-bars")
 	})
-	$(".btnlink").click(function(){
-		var pid=$(this).attr("data")
-		if(pid=="all"){
-			window.location.href="projects.html"
-		}else{
-			window.location.href="projects.html#"+pid
-		}
+	ScrollOut({
+		targets:".career_path_div p,.skills_explanation label,.services_div li,.intro_div_info h4,.intro_div_info p,.skill_div",
+		threshold:.4
 	})
-	$(".btnblog").click(function(){
-		alert("services soon")
-	})
-	$(".btngetcv").click(function(){
-		window.open("files/ERIC GICHURI MUCHIRI CV.pdf")
-	})
-	$(".btnhire").click(function(){
-		$("#myModal").show()
-	})
-	$(".btnprojects").click(function(){
-		window.location.href="projects.html"
-	})
-	$(".btnclosemodal").click(function(){
-		$("#myModal").hide()
-	})
-	$(".btncall").click(function(){
-		window.location.href="tel:+254707273244"
-	})
-	$(".btnemail").click(function(){
-		window.location.href="mailto:ericgichuri2605@gmail.com"
-	})
-
 })
-
-gsap.from('body',{duration:0,display:'none',delay:0,ease:'bounce'})
-gsap.from('.landing',{duration: 0.3, x:'-100vw',background:'#000088' ,delay:0.0,ease:'power2.in'})
-gsap.from('.text-landing',{duration:0.4,y:'50px',delay:0.5,opacity:0,ease:'power'})
-gsap.from('.img1',{duration:0.4,y:'50px',delay:0.4,opacity:0,ease:'power'})
-gsap.to('.landing',{duration: 0.5, height:'0px',display:'none',delay:1.5,ease:'power2.in'})
-gsap.from('.navbar',{duration: 2,display:'none',opacity:'0',delay:3,ease:'power'})
-gsap.from('.container1',{duration:0.4,display:'none',y:'100%',delay:4,ease:'power2.inOut'})
-gsap.from('.container1 .subcontainer1',{duration:0.8,x:'100vw',delay:4.4,ease:'power'})
-gsap.from('.container1 .subcontainer2',{duration:0.8,x:'-100vw',delay:5.7,ease:'power'})
-gsap.from('.container1 .subcontainer3',{duration:0.8,x:'100vw',delay:5.8,ease:'power'})
-gsap.from('.container1 .subcontainer4',{duration:0.8,x:'-100vw',delay:5.9,ease:'power'})
-
-gsap.from('img',{'duration':0.5,x:'-20px',opacity:0,delay:6,ease:'power2.in',stagger:.1})
-gsap.from('button',{'duration':0.5,x:'100vw',delay:6,ease:'power2.inOut',stagger:.2})
-
-gsap.from('.container1 h3',{duration:0.6,y:'50px',delay:4.4,opacity:0,ease:'power',stagger:.3})
-gsap.from('.container1 h4',{duration:0.6,y:'50px',delay:5.1,opacity:0,ease:'power',stagger:.3})
-gsap.from('.container1 h5',{duration:0.6,y:'50px',delay:5.7,opacity:0,ease:'power',stagger:.3})
-gsap.from('.container1 h6',{duration:0.6,y:'50px',delay:5.9,opacity:0,ease:'power',stagger:.3})
-gsap.from('.container1 p',{duration:0.6,y:'50px',delay:6.1,opacity:0,ease:'power',stagger:.3})
-
-gsap.from('.footer',{duration:2,display:'none',delay:6,ease:'power2.in'})
