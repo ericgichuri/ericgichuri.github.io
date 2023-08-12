32e20afc1d2a2f51aab1d7071f7a7089ae6780d8
Eric Gichuri <ericgichuri2605@gmail.com>
2023-08-12 17:11:59 +0300

    updates
    favicon

--- scripts/index.js
+++ scripts/index.js
@@ -7,4 +7,22 @@ $(document).ready(function(){
 		targets:".career_path_div p,.skills_explanation label,.services_div li,.intro_div_info h4,.intro_div_info p,.skill_div",
 		threshold:.4
 	})
+	$(".btn_cv").click(function(){
+		window.open("files/ERIC GICHURI MUCHIRI CV.pdf")
+	})
+	$(".btn_projects").click(function(){
+		window.open("projects.html")
+	})
+	$(".btn_hire").click(function(){
+		$("#myModal").show()
+	})
+	$(".btnclosemodal").click(function(){
+		$("#myModal").hide()
+	})
+	$(".btncall").click(function(){
+		window.open("tel:+254707273244")
+	})
+	$(".btnemail").click(function(){
+		window.open("mailto:ericgichuri2605@gmail.com")
+	})
 })
